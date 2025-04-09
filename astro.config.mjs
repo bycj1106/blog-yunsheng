b22905919aaa54131696e3d109ba476c1a01e41b
云升 <bycj1106@163.com>
2025-04-09 14:30:00 +0800

    feat: 移除 Partytown 配置并直接加载 Google Analytics 脚本

--- astro.config.mjs
+++ astro.config.mjs
@@ -61,11 +61,6 @@ export default defineConfig({
         Passed: async () => true, // https://github.com/PlayForm/Compress/issues/376
       },
     }),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
   ],
   markdown: {
     remarkPlugins: [
