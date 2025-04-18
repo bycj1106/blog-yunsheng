b1aa89feb4ae0c297cb06475ec59006f0ddafe01
云升 <bycj1106@163.com>
2025-04-18 14:54:56 +0800

    feat: 统一代码格式，优化配置文件和类型定义

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,17 @@
 import { defineCollection, z } from "astro:content";
 
 const postsCollection = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    published: z.date(),
-    updated: z.date().optional(),
-    draft: z.boolean().optional().default(false),
-    comments: z.boolean().optional().default(true),
-    description: z.string().optional().default(''),
-    image: z.string().optional().default(''),
-    tags: z.array(z.string()).optional().default([]),
-    category: z.string().optional().default(''),
-    lang: z.string().optional().default(''),
+	schema: z.object({
+		title: z.string(),
+		published: z.date(),
+		updated: z.date().optional(),
+		draft: z.boolean().optional().default(false),
+		comments: z.boolean().optional().default(true),
+		description: z.string().optional().default(""),
+		image: z.string().optional().default(""),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().default(""),
+		lang: z.string().optional().default(""),
 
 		/* For internal use */
 		prevTitle: z.string().default(""),
